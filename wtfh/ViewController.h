57d680f97012f0e97067d098f745f37290cd5b6f
Brian <laxnski@gmail.com>
2014-01-09 22:25:43 -0800

    Initial Commit

---
+++ wtfh/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wtfh
+//
+//  Created by Brian Anglin on 1/9/14.
+//  Copyright (c) 2014 Brian Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ wtfhTests/wtfhTests.m
@@ -0,0 +1,34 @@
+//
+//  wtfhTests.m
+//  wtfhTests
+//
+//  Created by Brian Anglin on 1/9/14.
+//  Copyright (c) 2014 Brian Anglin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wtfhTests : XCTestCase
+
+@end
+
+@implementation wtfhTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
